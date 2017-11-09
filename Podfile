6e42a1677054e7ec42708c83cd2ea003c7bd6ae0
zhangzhan <zhangzhan@firedata.cc>
2017-11-09 17:38:24 +0800

    SDK Demo初始化

---
+++ Podfile
@@ -0,0 +1,13 @@
+# Uncomment the next line to define a global platform for your project
+platform :ios, '8.0'
+
+target 'botbrain_feed_sdk_ios_demo' do
+  # Uncomment the next line if you're using Swift or would like to use dynamic frameworks
+  # use_frameworks!
+  # ignore all warnings from all pods
+  inhibit_all_warnings!
+  # Pods for botbrain_feed_sdk_ios_demo
+  # 二选一
+  pod 'botbrain_feed_sdk_ios'
+  # pod 'botbrain_feed_sdk_ios_lite'
+end
--- botbrain_feed_sdk_ios_demo/ViewController.m
+++ botbrain_feed_sdk_ios_demo/ViewController.m
@@ -3,10 +3,11 @@
 //  botbrain_feed_sdk_ios_demo
 //
 //  Created by ZHAN on 2017/11/7.
-//  Copyright © 2017年 BotBrain. All rights reserved.
+//  Copyright © 2017年 BotBrain. All righ.
 //
 
 #import "ViewController.h"
+#import "BOTContentViewController.h"
 
 @interface ViewController ()
 
@@ -14,15 +15,25 @@
 
 @implementation ViewController
 
+#pragma mark - life cycle
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    self.navigationController.navigationBar.tintColor = [UIColor whiteColor];
 }
-
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+}
+
+#pragma mark - create views
+
+
+#pragma mark - event response
+- (IBAction)showFeed:(UIButton *)sender {
+    BOTContentViewController *contentViewController = [[BOTContentViewController alloc] init];
+    [self.navigationController pushViewController:contentViewController animated:YES];
 }
 
 
